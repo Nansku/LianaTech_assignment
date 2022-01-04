86bd8bc4aa3993f58c7bb77c3c72bbd9e277b949
n9peje00 <n9peje00@students.oamk.fi>
2022-01-04 20:34:18 +0200

    Unused imports deleted

--- lianatech_modified/src/components/Infobar.js
+++ lianatech_modified/src/components/Infobar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, Row } from 'react-bootstrap';
+import { Navbar, Nav, Form } from 'react-bootstrap';
 
 const Infobar = () => {
     return (
--- lianatech_modified/src/components/PopUp.js
+++ lianatech_modified/src/components/PopUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Button } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 
 function PopUp(props) {
   return (

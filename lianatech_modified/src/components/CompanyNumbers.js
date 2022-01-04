567907ccd93c8097edda0d08d3d6190089f4ec42
n9peje00 <n9peje00@students.oamk.fi>
2022-01-04 20:43:30 +0200

    RWD fix for numbers

--- lianatech_modified/src/components/CompanyNumbers.js
+++ lianatech_modified/src/components/CompanyNumbers.js
@@ -10,31 +10,31 @@ const CompanyNumbers = () => {
 
     return (
         <div className="container-fluid d-flex justify-content-center pt-5 pb-2">
-            <Row className="align-items-center">
-                <Col xs={5} sm={1} className="d-flex justify-content-end">
+            <Row className="align-items-center ">
+                <Col xs={5} md={1} className="d-flex justify-content-end">
                     <img src={business} />
                 </Col>
 
-                <Col xs={7} sm={3}>
+                <Col xs={7} md={3}>
                     
                     <CountUp className="countUpNumbers" isCounting start={3000} end={Number.MAX_SAFE_INTEGER / 10} duration={Number.MAX_SAFE_INTEGER} />
                     <p>Clients</p>
                 </Col>
 
-                <Col xs={5} sm={1} className="ps-5 d-flex justify-content-end">
+                <Col xs={5} md={1} className="ps-5 d-flex justify-content-end">
                     <img src={face} />
                 </Col>
 
-                <Col xs={7} sm={3}>
+                <Col xs={7} md={3}>
                     <CountUp className="countUpNumbers" isCounting start={180} end={Number.MAX_SAFE_INTEGER / 5} duration={Number.MAX_SAFE_INTEGER} />
                     <p>Employees</p>
                 </Col>
 
-                <Col xs={5} sm={1} className="ps-5 d-flex justify-content-end">
+                <Col xs={5} md={1} className="ps-5 d-flex justify-content-end">
                     <img src={earth} />
                 </Col>
 
-                <Col xs={7} sm={2}>
+                <Col xs={7} md={2}>
                     <CountUp className="countUpNumbers" isCounting start={10000} end={Number.MAX_SAFE_INTEGER / 3} duration={Number.MAX_SAFE_INTEGER} />
                     <p>Daily users</p>
                 </Col>
